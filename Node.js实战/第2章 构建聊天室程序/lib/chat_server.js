c2b208ade7fb612da2eabecf5ef4296cc2f79d4d
fridolph <249121486@qq.com>
2016-09-19 15:41:16 +0800

    nodejs实战

--- Node.js实战/第2章 构建聊天室程序/lib/chat_server.js
+++ Node.js实战/第2章 构建聊天室程序/lib/chat_server.js
@@ -1,7 +1,7 @@
 /* 
 * @Author: fys
 * @Date:   2016-09-18 11:19:35
-* @Last Modified time: 2016-09-18 13:49:42
+* @Last Modified time: 2016-09-18 15:12:31
 */
 
 var socket = require('socket.io'),
@@ -44,4 +44,62 @@ exports.listen = function(server) {
     handleClientDisconnection(socket, nickNames, nameUsed)
 
   })
+}
+
+/* 分配用户昵称 */
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+
+  // 生成新昵称
+  var name = 'Guest' + guestNumber;
+  // 把用户昵称跟客户端连接ID关联上
+  nickNames[socket.id] = name;
+
+  // 让永固知道他们的昵称
+  socket.emit('nameResult', {
+    success: true,
+    name: name
+  })
+
+  // 存放已经存在的昵称
+  namesUsed.push(name)
+  // 增加用来生成昵称的计数器
+  return guestNumber + 1
+}
+
+/* 进入聊天室的相关逻辑 */
+function joinRoom(socket, room) {
+
+  // 让用户进入房间
+  socket.join(room)
+
+  // 记录用户的当前房间
+  currentRoom[socket.id] = room
+
+  socket.emit('joinResult', {room: room})
+
+  // 让房间里的其他用户知道有新用户进入了房间
+  socket.broadcast.to(room).emit('message', {
+    text: nickNames[socket.id] + ' has joined ' + room + '.'
+  })
+
+  // 确定有哪些用户在这个房间里
+  var usersInRoom = io.sockets.clients(room)
+
+  // 如果不止一个用户在这个房间里，汇总下都是谁
+  if (usersInRoom.length > 1) {
+    var usersInRoomSummary = 'Users Currently in ' + room + ': ';
+
+    for(var index in usersInRoom) {
+      var userSocketId = usersInRoom[index].id;
+      if (userSocketId !== socket.id) {
+        if (index > 0) {
+          usersInRoomSummary += ', ';
+        }
+        usersInRoomSummary += nickNames[userSocketId];
+      }
+    }
+    usersInRoomSummary += '.';
+    // 将房间里其他用户的汇总发送给这个用户
+    socket.emit('message', {text: usersInRoomSummary});
+  }
 }
